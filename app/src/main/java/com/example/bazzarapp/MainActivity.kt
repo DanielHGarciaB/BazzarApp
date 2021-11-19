e82d3a4fc7bd3d1463cb81fc81098db03094332f
Daniel H Garcia <danielhgarciab@gmail.com>
2021-11-18 19:42:53 -0500

    DG2021111801

--- app/src/main/java/com/example/bazzarapp/MainActivity.kt
+++ app/src/main/java/com/example/bazzarapp/MainActivity.kt
@@ -6,11 +6,20 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.facebook.CallbackManager
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 class MainActivity : AppCompatActivity() {
     private var edtUsername: EditText? = null
     private var edtPassword: EditText? = null
+
+    private val GOOGLE_SING_IN = 100
+    private val callbackManager = CallbackManager.Factory.create()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         //Splash
         //setTheme(R.style.SplashScreen)
@@ -78,5 +87,40 @@ class MainActivity : AppCompatActivity() {
         ).show();
     }
 
+    fun googleLogin(view: android.view.View) {
 
+        val googleConf = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(resources.getString(R.string.default_web_client_id2))
+            .requestEmail().build()
+
+        val googleClient = GoogleSignIn.getClient(this, googleConf)
+        googleClient.signOut()
+        startActivityForResult(googleClient.signInIntent, GOOGLE_SING_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == GOOGLE_SING_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+
+            try {
+                val account = task.getResult(ApiException::class.java)
+                if (account != null) {
+                    val credencial = GoogleAuthProvider.getCredential(account.idToken, null)
+                    FirebaseAuth.getInstance().signInWithCredential(credencial)
+                        .addOnCompleteListener {
+                            if (it.isSuccessful) {
+                                showHome(account.email ?: "", ProviderType.GOOGLE)
+                            } else {
+                                getToast("Failed to authenticate");
+                            }
+                        }
+                }
+            } catch (e: ApiException) {
+                getToast("Failed to authenticate");
+            }
+        }
+    }
 }
